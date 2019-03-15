ab611b8d55cc76cab15e67ef112e4d218947f63c
tiansuyyan <tiansuyan@k2data.com.cn>
2019-03-15 15:31:24 +0800

    create

---
+++ app/app.js
@@ -0,0 +1,28 @@
+import Modeler from 'bpmn-js/lib/Modeler';
+
+import PropertiesPanel from './properties-panel';
+
+import customModdleExtension from './moddle/custom.json';
+
+import diagramXML from './diagram.bpmn';
+
+const $modelerContainer = document.querySelector('#modeler-container');
+const $propertiesContainer = document.querySelector('#properties-container');
+const $download = document.querySelector('#download');
+
+const modeler = new Modeler({
+  container: $modelerContainer,
+  moddleExtensions: {
+    custom: customModdleExtension
+  },
+  keyboard: {
+    bindTo: document.body
+  }
+});
+
+const propertiesPanel = new PropertiesPanel({
+  container: $propertiesContainer,
+  modeler
+});
+
+modeler.importXML(diagramXML);
